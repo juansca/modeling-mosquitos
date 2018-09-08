feb9fb8ed555e1b18e7ace41a12c2fc42724f0d2
juansca <juansca1229@gmail.com>
2018-09-08 16:18:46 -0300

    Fix typos bugs on setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-sfrom setuptools import find_packages, setup
+from setuptools import find_packages, setup
 
 setup(
     name="mosquitomodeling",
@@ -9,13 +9,13 @@ setup(
     include_package_data=True,
     packages=find_packages(),
     install_requires=[
-        "tabulate==0.8.2"
+        "tabulate==0.8.2",
         "docopt==0.6.2",
         "numpy==1.14.5",
         "matplotlib==2.2.2",
         "scikit-learn==0.19.2",
         "scipy==1.1.0",
-        "pandas==0.23.4"
+        "pandas==0.23.4",
         "seaborn==0.8.1",
         "rpy2==2.9.4",
         "tzlocal==1.5.1"
