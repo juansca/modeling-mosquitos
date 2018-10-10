5583e96c47eb798f8787259a8cbeee377db34894
¨jscavuzzo¨ <juan.scavuzzo@mercadolibre.com>
2018-10-10 20:56:42 -0300

    Fix bug related to clean_data path

--- .clean_data.sh
+++ .clean_data.sh
@@ -8,15 +8,15 @@ rm data/* -f
 rm instances/* -f
 
 # Instances for tunning
-python data/clean_data.py -i data/raw_data/clorinda.csv -o tunning/instances/ --instances $INSTANCES --overlap $OVERLAPPING
-python data/clean_data.py -i data/raw_data/pampa.csv -o tunning/instances/ --instances $INSTANCES --overlap $OVERLAPPING
-python data/clean_data.py -i data/raw_data/iguazu.csv -o tunning/instances/ --instances $INSTANCES --overlap $OVERLAPPING
+python data/scripts/clean_data.py -i data/raw_data/clorinda.csv -o tunning/instances/ --instances $INSTANCES --overlap $OVERLAPPING
+python data/scripts/clean_data.py -i data/raw_data/pampa.csv -o tunning/instances/ --instances $INSTANCES --overlap $OVERLAPPING
+python data/scripts/clean_data.py -i data/raw_data/iguazu.csv -o tunning/instances/ --instances $INSTANCES --overlap $OVERLAPPING
 
 # Cleaned data to train and eval models
-python data/clean_data.py -i data/raw_data/tartagal.csv -o data/cleaned_data/tartagal/
-python data/clean_data.py -i data/raw_data/clorinda.csv -o data/cleaned_data/clorinda/
-python data/clean_data.py -i data/raw_data/pampa.csv -o data/cleaned_data/pampa/
-python data/clean_data.py -i data/raw_data/iguazu.csv -o data/cleaned_data/iguazu/
+python data/scripts/clean_data.py -i data/raw_data/tartagal.csv -o data/cleaned_data/tartagal/
+python data/scripts/clean_data.py -i data/raw_data/clorinda.csv -o data/cleaned_data/clorinda/
+python data/scripts/clean_data.py -i data/raw_data/pampa.csv -o data/cleaned_data/pampa/
+python data/scripts/clean_data.py -i data/raw_data/iguazu.csv -o data/cleaned_data/iguazu/
 
 # Trap ctrl-c and other exit signals and delete all temporary files
 trap TrapError 1 2 3 15;
